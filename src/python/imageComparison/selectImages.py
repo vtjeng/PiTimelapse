0a93a06e8cbfafcb1b839cdc185e0dcc1292bfae
Vincent Tjeng <vtjeng@mit.edu>
2015-02-08 11:36:18 -0500

    Modified select image, and added java source directory.

---
+++ src/python/imageComparison/selectImages.py
@@ -0,0 +1,45 @@
+__author__ = 'vince_000'
+
+import cv2
+import os
+import itertools
+import cProfile
+
+workingDirectory = "D:\Timelapse Project\Suite Lounge III - Working Copy"
+sampleSize = 50
+
+os.chdir(workingDirectory)
+
+fileList = os.listdir(workingDirectory)[0:sampleSize]
+
+# not used in this situation
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return itertools.izip(a, b)
+
+def calculate_diffs(fileList):
+    # calculates the differences between successive images
+
+    thisImage = cv2.imread(fileList[0], cv2.IMREAD_COLOR)
+    height, width = thisImage.shape[:2]
+    diffs=[]
+    for path in fileList[1:len(fileList)]:
+        nextImage = cv2.imread(path, cv2.IMREAD_COLOR)
+        diffs.append(cv2.norm(thisImage, nextImage)/(height*width))
+        thisImage = nextImage
+    return diffs
+
+# def calculate_image_distance(img_path_1, img_path_2):
+#     return cv2.norm(
+#         *map(lambda x: cv2.imread(x, cv2.IMREAD_COLOR), [img_path_1, img_path_2])
+#     )
+
+# print(calculate_diffs(fileList))
+
+cProfile.run("calculate_diffs(fileList)")
+# benchmark times: 0.178 to import, 0.016 to calculate norms.
+
+
+
